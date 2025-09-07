80cf56825872261164976c4dd228d8ccf6313959
Ahsan Habib <habib23105341021@diu.edu.bd>
2025-09-07 06:03:10 +0600

    Modal Done

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+const createElement = (arr) =>{
+    const htmlElements = arr.map(element => `<span class="btn">${element}</span>`)
+    return htmlElement;
+}
+
+
+
+
+
 const loadLessons = () => {
     const url = "https://openapi.programming-hero.com/api/levels/all";
     fetch(url)
@@ -47,37 +56,88 @@ const loadCard = (id) => {
             const activeBtn = document.getElementById(`lesson-btn${id}`);
             deactiveBtn(element.data);
             activeBtn.classList.add("active");
-           
+
         })
 }
 
 
-const deactiveBtn = element =>{
+const deactiveBtn = element => {
     const allBtn = document.querySelectorAll(".btn-all");
     allBtn.forEach(element => {
         element.classList.remove("active");
     });
 }
 
-  const wordDetails = async(id) =>{
-        const url = `https://openapi.programming-hero.com/api/word/${id}`;
-        const res = await fetch(url);
-        const detials = await res.json();
-        displayWordDetails(detials.data);
-    }
+const wordDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+    const res = await fetch(url);
+    const detials = await res.json();
+    displayWordDetails(detials.data);
+}
 
-    const displayWordDetails = data => {
-        console.log(data)
-        const modal = document.getElementById('detail-box');
-        modal.innerHTML =`
-        <h1> Hello </h1>
+const displayWordDetails = data => {
+    console.log(data)
+    const modal = document.getElementById('detail-box');
+    modal.innerHTML = `
+      <div>
+                    <div class="my-3">
+                        <h1 class="font-bold text-4xl">${data.word} ( <i class="fa-solid fa-microphone-lines"></i>  :${data.pronunciation})</h1>
+                    </div>
+                    <div>
+                        <h1 class="text-2xl font-semibold">Meaning</h1>
+                        <p class="font-bangla font-medium text-2xl">${data.meaning}</p>
+                    </div>
+                    <div class="my-3">
+                        <h1 class="text-2xl font-semibold">Example</h1>
+                        <p class="font-bangla font-medium text-2xl" >${data.sentence}</p>
+                    </div>
+
+                    <div>
+                        <h1>Synnonames</h1>
+                        <div>${createElement(data.synonyms)}</div>
+                    </div>
+                </div>
         `
-        modal_word.showModal()
-    }
+    modal_word.showModal()
+
+    console.log(data)
+}
 
 
 
-
+// id
+// : 
+// 5
+// level
+// : 
+// 1
+// meaning
+// : 
+// "আগ্রহী"
+// partsOfSpeech
+// : 
+// "adjective"
+// points
+// : 
+// 1
+// pronunciation
+// : 
+// "ইগার"
+// sentence
+// : 
+// "The kids were eager to open their gifts."
+// synonyms
+// : 
+// Array(3)
+// 0
+// : 
+// "enthusiastic"
+// 1
+// : 
+// "excited"
+// 2
+// : 
+// "keen"
 
 
 
@@ -86,7 +146,7 @@ const deactiveBtn = element =>{
 const displayCard = (data) => {
     const card = document.getElementById('card-container');
     card.innerHTML = '';
-    if(data.length == 0){
+    if (data.length == 0) {
         card.innerHTML = `   <div class="text-center col-span-3 font-bangla">
             <img class="m-auto" src="./assets/alert-error.png" alt="">
              <p class="text-[14px] text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।!!</p>
@@ -95,7 +155,7 @@ const displayCard = (data) => {
     }
 
     data.forEach(element => {
-         const div = document.createElement("div");
+        const div = document.createElement("div");
         div.innerHTML = `
           <div class="card bg-white py-14 px-10 rounded-xl  bg-base-100 shadow-sm text-center h-full" id="cardDiv">
             <h2 class="mb-4 text-3xl font-bold">${element.word ? element.word : "Word Not Found"}</h2>
